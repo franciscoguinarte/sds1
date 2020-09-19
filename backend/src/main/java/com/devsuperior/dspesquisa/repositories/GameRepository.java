c64fefa13422af2a30d1bec53ed7b6a4e7272e83
Francisco Filho <francisco.guinarte@outlook.com.br>
2020-09-19 19:20:16 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dspesquisa.entities.Game;
+
+public interface GameRepository extends JpaRepository< Game ,Long> {
+	
+
+}
